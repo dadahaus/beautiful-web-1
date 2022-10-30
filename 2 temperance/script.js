7979232a0bc458fa4cae1749aa023fe165f96e53
kosme <casadadaista@gmail.com>
2022-10-30 09:35:51 -0300

    removed large files

--- 2 temperance/script.js
+++
@@ -1,7 +0,0 @@
-const hamburguer = document.getElementById('hamburguer');
-const nav = document.getElementById('nav');
-
-hamburguer.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    hamburguer.classList.toggle('active');
-});
